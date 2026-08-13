4604473515d8f303098eebcc274aa3f26352a1d8
gopalakrishnan-chakkaravarthy <gopalakrishnan.chakkaravarthy@hotmail.com>
2026-08-13 11:22:29 +0530

    Updated server ts file

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ import {
 import { convertToFHIRDiagnosticReport } from './server/fhirEngine';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 
